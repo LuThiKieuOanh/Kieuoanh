77f45632b875665494116f8ad777487cd2223d7f
LuThiKieuOanh <lukieuoanh163@gmail.com>
2024-08-17 14:24:21 +0700

    Add files via upload

---
+++ diemtrungbinh.py
@@ -0,0 +1,22 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Aug 17 13:44:42 2024
+
+@author: KieuOanh
+"""
+
+GPA=float(input("nhap diem trung binh (GPA):"))
+if GPA<3.5:
+    print("hoc luc kem")
+elif GPA>=3.5 and GPA<5.0:
+    print("hoc luc yeu")
+elif GPA>=5.0 and GPA<7.0:
+    print("hoc luc trung binh")
+elif GPA>=7.0 and GPA<8.0:
+    print("hoc luc kha")
+elif GPA>=8.0 and GPA<9.0:
+    print("hoc luc gioi")
+elif GPA>=9.0 and GPA<10:
+    print("hoc luc xuat sac")
+    
+
